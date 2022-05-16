ee9127194e5ae1a4e1ba888fe2b6de83a386319b
Naptoss <tomgabinio@gmail.com>
2022-05-16 16:08:45 -0300

    Sobre

--- projeto/urls.py
+++ projeto/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-     path('produto/', include('produto.urls'))
+     path('produto/', include('produto.urls')),
+     path('sobre/',include('sobre.urls')),
 ]
  
